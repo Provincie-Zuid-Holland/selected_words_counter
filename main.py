2aba5e2bc51e68ecf7b0c4ad719e1cacfded816b
Michael de Winter <mr.de.winter@pzh.nl>
2024-10-28 11:33:51 +0100

    For better extracting refactor

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-import datetime
-from datetime import datetime
+import argparse
+import os
 from glob import glob
 
 import config
-from selected_words_counter import counting
-
-version = 3
-
-
-def generate_filename(version):
-    current_date = datetime.now().strftime("%Y-%m-%d")  # Format as YYYY-MM-DD
-    filename = f"./output/selected_word_counter_{current_date}_v{version}.xlsx"
-    return filename
-
-
-df = counting.word_counting_in_files(
-    config.aword_list,
-    [
-        afilepath
-        for afilepath in glob(config.local_folder_mount_point_extracted + "/*.txt")
-    ],
+from selected_words_counter import (
+    SelectedWordCounter,
+    counting,
+    extract_files,
+    functions,
 )
 
-df.to_excel(generate_filename(version), index=False)
+version = 4
+
+
+def main():
+    # Create an argument parser
+    parser = argparse.ArgumentParser(
+        description="Override config parameters when using this program from the command line."
+    )
+
+    # Add arguments for the config parameters you want to override
+    parser.add_argument("--aword_list", type=str, help="Words array to be counted")
+    parser.add_argument(
+        "--local_folder_mount_point", type=str, help="The directory to read files from"
+    )
+    parser.add_argument(
+        "--keep_extracted", type=str, help="Wether to keep the extracted .txt files."
+    )
+    parser.add_argument("--output_dir", type=str, help="Where to store the output file")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Override config parameters if they are provided in the command line
+    if args.aword_list:
+        # Lower case all words for easier matching.
+        aword_list = [aword.lower().replace(".", "") for aword in args.aword_list]
+        config.aword_list = aword_list
+    if args.local_folder_mount_point:
+        config.local_folder_mount_point = args.local_folder_mount_point
+    if args.keep_extracted:
+        config.keep_extracted = args.keep_extracted
+    if args.output_dir:
+        config.output_dir = args.output_dir
+
+    aselected_words_counter_class = SelectedWordCounter()
+    aselected_words_counter_class.run()
+
+
+if __name__ == "__main__":
+    main()
--- src/selected_words_counter/extract_files.py
+++ src/selected_words_counter/extract_files.py
@@ -1,8 +1,10 @@
 import os
 import re
+import zipfile
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from glob import glob
 
+import extract_msg
 from tqdm import tqdm
 
 import config
@@ -39,6 +41,39 @@ def process_and_save_file(afilepath, verbose=False):
         print(f"Error processing {afilepath}: {e}")
 
 
+def extract_msg_attachments():
+    # Extract all .msg files into a directory
+    for afilepath in glob(config.local_folder_mount_point + "*.msg"):
+        try:
+            msg = extract_msg.Message(afilepath)
+
+            if len(msg.attachments) > 0:
+                a_output_directory = afilepath.rsplit(".", 1)[0]
+                print("Making :" + str(a_output_directory))
+                os.makedirs(a_output_directory)
+
+                for item in range(0, len(msg.attachments)):
+                    att = msg.attachments[item]
+                    msg.attachments[item].save(
+                        customPath=a_output_directory, customFilename=att.longFilename
+                    )
+        except Exception as e:
+            print(e)
+
+
+def extract_zip_attachments():
+    # Extract a zip file into a new directory
+    for afilepath in glob(config.local_folder_mount_point + "*.zip"):
+        print(afilepath)
+        a_output_directory = str(afilepath.rsplit(".", 1)[0])
+        print("Making directory to save files in:" + a_output_directory)
+        os.makedirs(a_output_directory)
+
+        # Extract the contents of the zip file
+        with zipfile.ZipFile(afilepath, "r") as zip_ref:
+            zip_ref.extractall(a_output_directory)
+
+
 def extracted_files_from_list_filepaths(afilepaths, verbose=False):
     with ThreadPoolExecutor() as executor:
         futures = {
@@ -53,6 +88,11 @@ def extracted_files_from_list_filepaths(afilepaths, verbose=False):
 
 
 def run():
+    # First extract all the .msg files.
+    extract_msg_attachments()
+    # Then extract all the .zip files.
+    extract_zip_attachments()
+
     if os.path.isdir(config.local_folder_mount_point_extracted) == False:
         os.makedirs(config.local_folder_mount_point_extracted)
 
