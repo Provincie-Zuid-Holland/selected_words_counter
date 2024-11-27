0bbd42f5de58f0cbb33f5f1c9d338965780c0c7b
Michael de Winter <mr.de.winter@pzh.nl>
2024-11-27 10:19:07 +0100

    Multithreading off on default

--- config.py
+++ config.py
@@ -19,16 +19,14 @@ target_dir = "./data/"
 # Directory where to write the converted data to.
 target_dir_extracted = target_dir[::-1].replace("/", "", 1)[::-1] + "_converted/"
 
-
 # Extracting settings.
 # Wether to delete converted data.
-keep_extracted = False
-# Wether to multithread read in of files, a possible speed up but currently not always working
-multi_thread = True
+keep_extracted = True
+# Wether to multithread read in of files, a possible speed up but currently not always working TODO: Figure out why this is not working
+multi_thread = False
 
 # Set this to false if files have already been extracted.
 extract = True
-
 
 # Output Settings.
 # Directory to output data to.
